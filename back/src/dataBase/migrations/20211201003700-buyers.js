b53e5f75e87f930b22935161c624f85875bf864a
Jefferson <jffrsncrls@gmail.com>
2021-11-30 22:58:32 -0300

    feat: criada a tabela buyers

--- back/src/dataBase/migrations/20211201003700-buyers.js
+++ back/src/dataBase/migrations/20211201003700-buyers.js
@@ -1,21 +1,117 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable("buyers", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER(11),
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+      tradingName: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      cashforceTax: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      responsibleName: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      responsibleEmail: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      responsiblePosition: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      responsiblePhone: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      responsibleMobile: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      website: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      postalCode: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      address: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      number: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      complement: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      neighborhood: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      city: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      state: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      phoneNumber: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      situation: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      situationDate: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+      createdAt: {
+        type:Sequelize.DATE,
+        allowNull:false,
+      },
+      updatedAt: {
+        allowNull:false,
+        type: Sequelize.DATE,
+      },
+      cnpjId: {
+        type: Sequelize.INTEGER(11),
+        defaultValue: null,
+      },
+      confirm: {
+        defaultValue:1,
+        type: Sequelize.TINYINT(1),
+      },
+      email: {
+        defaultValue: null,
+        type: Sequelize.STRING(255),
+      },
+    },
+
+    {
+      engine: 'InnoDB',
+      charset: 'latin1'
+    }
+
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable("users");
+  },
 };
