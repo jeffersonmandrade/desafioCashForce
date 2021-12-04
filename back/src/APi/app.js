7c19c8d11f0aded0062c0725de7359370e2411b4
Jefferson <jffrsncrls@gmail.com>
2021-12-04 18:39:23 -0300

    feat: criado a Api

---
+++ back/src/APi/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const propostasRoute  = require('../routes/propostaRoute')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/propostas',propostasRoute)
+
+module.exports = app;
