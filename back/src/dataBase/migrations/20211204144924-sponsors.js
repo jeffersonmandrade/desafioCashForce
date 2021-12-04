5d10f4f38feff0de35f225629daf6d10ee491194
Jefferson <jffrsncrls@gmail.com>
2021-12-04 11:56:49 -0300

    feat: criado a migration sponsors

---
+++ back/src/dataBase/migrations/20211204144924-sponsors.js
@@ -0,0 +1,129 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      "sponsors",
+      {
+        id: {
+          primaryKey: true,
+          autoIncrement: true,
+          type: Sequelize.INTEGER(11),
+        },
+        name: {
+          allowNull: false,
+          type: Sequelize.STRING(255)
+        },
+        tradingName: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        cashforceTax: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        responsibleName: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        responsibleEmail:{
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        responsiblePosition:{
+          defaultValue: null,
+          type: Sequelize.STRING(255)
+        },
+        responsiblePhone:{
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        responsibleMobile:{
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        website: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        postalCode:{
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        address: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        number: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        complement: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        neighborhood: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        city:{
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        state: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        bank: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        bankAgency:{
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        account: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        phoneNumber: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        } ,
+        situation: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        situationDate: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        },
+        createdAt: {
+          type:Sequelize.DATE,
+          allowNull:false,
+        },
+        updatedAt: {
+          allowNull:false,
+          type: Sequelize.DATE,
+        },
+        cnpjId: {
+          defaultValue:null,
+          type: Sequelize.INTEGER(11),
+        },
+        email: {
+          defaultValue: null,
+          type: Sequelize.STRING(255),
+        }
+      },
+      {
+        engine: "InnoDB",
+        charset: "latin1",
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("sponsors");
+  },
+};
+
