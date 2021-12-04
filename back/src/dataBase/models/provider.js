a9a3c878b2f53fd557086961a8e71f1926304c02
Jefferson <jffrsncrls@gmail.com>
2021-12-04 15:28:03 -0300

    feat: criado o model provider.

---
+++ back/src/dataBase/models/provider.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const Provider = sequelize.define("provider", {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: DataTypes.STRING(255),
+    tradingName: DataTypes.STRING(255),
+    cashforceTax:DataTypes.STRING(255),
+    responsibleName: DataTypes.STRING(255),
+    responsibleEmail: DataTypes.STRING(255),
+    responsiblePosition:DataTypes.STRING(255),
+    responsiblePhone: DataTypes.STRING(255),
+    responsibleMobile: DataTypes.STRING(255),
+    website: DataTypes.STRING(255),
+    postalCode:DataTypes.STRING(255),
+    address:DataTypes.STRING(255),
+    number: DataTypes.STRING(255),
+    complement:DataTypes.STRING(255),
+    neighborhood: DataTypes.STRING(255),
+    city: DataTypes.STRING(255),
+    state: DataTypes.STRING(255),
+    bank: DataTypes.STRING(255),
+    bankAgency:DataTypes.STRING(255),
+    account:DataTypes.STRING(255),
+    documents:DataTypes.STRING(255),
+    phoneNumber:DataTypes.STRING(255),
+    situation:DataTypes.STRING(255),
+    situationDate:DataTypes.STRING(255),
+    cnpjId:DataTypes.INTEGER(11),
+    email:DataTypes.STRING(255),
+  });
+
+  return Provider;
+};
