988dcd335b6cd2aa0ab62dcc90aedec3cdd54e5a
Jefferson <jffrsncrls@gmail.com>
2021-12-05 20:34:36 -0300

    feat: criado o componente pageHeader.

---
+++ front/src/components/pageHeader/PageHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Vector from '../../image/Vector.svg'
+import * as S from './style'
+export default function PageHeader() {
+  return (
+    <S.main>
+      <S.span>
+        <S.img src={Vector} alt="icon azul"/>
+        <S.h3>Notas fiscais</S.h3>
+      </S.span>
+      <S.p>Visulize as notas fiscais que você tem.</S.p>
+
+    </S.main>
+  )
+}
---
+++ front/src/components/pageHeader/style.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const main = styled.main`
+display: flex;
+flex-wrap: wrap;
+align-items: flex-start;
+padding: 0px;
+position: absolute;
+width: 255px;
+height: 53px;
+left: 336px;
+top: 104px;
+background-color: red;
+`
+export const img = styled.img`
+position: absolute;
+left: 0%;
+right: 0%;
+top: 20.83%;
+bottom: 20.57%;
+border: 1px solid #021B51;
+box-sizing: border-box;
+`
+export const h3 = styled.h3`
+font-family: DM Sans;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 28px;
+color:rgba(2, 27, 81, 1);
+`
+export const p = styled.p`
+width: 255;
+height: 20px;
+color:rgba(114, 125, 148, 1);
+font-family: DM Sans;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 20px;
+`
+
+export const span = styled.span`
+display: flex;
+flex-wrap: wrap;
+width: 255;
+height: 20px;
+background-color: blue;
+`
