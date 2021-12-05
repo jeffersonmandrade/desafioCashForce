5ea3b6d89e0e615b8200dc892ef1575f55c6e82e
Jefferson <jffrsncrls@gmail.com>
2021-12-05 20:35:56 -0300

    feat: criado a funcao organizedResult.
    A funcao organiza o retorno da funcao com os dados degejados pelo front.

--- back/src/services/proposta.js
+++ back/src/services/proposta.js
@@ -1,10 +1,23 @@
 const { order } =require('../dataBase/models')
 
+const organizeResult = (bigArray) => {
+
+const result =  bigArray.map(({nNf,buyer,orderStatusBuyer,provider,emissionDate,value}) => ({
+notaFiscal:nNf,
+sacado: buyer.name,
+cedente: provider.name,
+emissao:emissionDate,
+valor: value,
+status: orderStatusBuyer,
+}))
+return result
+}
 
 const getAllOrders = async () => {
   try{
     const result = await order.findAll({include:['provider','buyer']});
-    return result;
+    const changeResult = organizeResult(result)
+    return changeResult;
   }catch(e){
     console.log(e)
   }
