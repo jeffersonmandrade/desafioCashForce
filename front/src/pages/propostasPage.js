ea042ed6df0fd35231f62c2edb44e91bc575065e
Jefferson <jffrsncrls@gmail.com>
2021-12-05 20:27:40 -0300

    feat:  criado a pagina propostasPages.

--- front/src/pages/propostasPage.js
+++ front/src/pages/propostasPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../components/header/Header'
-
+import SiderFinancer from '../components/SiderFinancer/SiderFinancer'
+import Table from '../components/table/Table'
+import PageHeader from '../components/pageHeader/PageHeader'
 export default function propostasPage() {
   return (
     <div>
       <Header />
+      <SiderFinancer/>
+      <PageHeader />
+      <Table/>
     </div>
   )
 }
